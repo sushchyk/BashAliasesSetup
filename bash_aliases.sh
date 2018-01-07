aca333c5d188b134a9f2f723d2e01d8fa0e776e2
Andriy Sushchyk <sushchyk12@gmail.com>
2018-01-07 12:57:13 +0200

    Initial commit

---
+++ bash_aliases.sh
@@ -0,0 +1,13 @@
+alias ping_aliases="echo PONG"
+alias test_aliases2="echo HELLO"
+# Example 1
+# alias cd="composer dump"
+
+# Example 2
+# t() {
+#   if [ -f vendor/bin/phpunit ]; then
+#     vendor/bin/phpunit "$@"
+#   else
+#     phpunit "$@"
+#   fi
+# }
